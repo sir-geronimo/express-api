927d118fd844fe7a6d350b836c78fc7c1db194c8
Enger Jimenez <ejimenez@oriontek.do>
2020-02-09 20:49:51 -0400

    Refactor Home controller

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,10 +1,53 @@
 import express, { Request, Response } from 'express';
 
-import ControllerBase from './../base/Controller.base';
+import IController from './../interfaces/IController.interface';
 
-export default class HomeController extends ControllerBase {
+export default class HomeController implements IController {
+    path: string;
+    router: express.Router;
+
     constructor() {
-        super('/', express.Router());
-        console.log(this.path);
+        this.path = '/';
+        this.router = express.Router();
+        this.initRoutes();
+    }
+
+    initRoutes(): void {
+        this.router.get(`${this.path}:id`, this.get);
+        this.router.get(this.path, this.getMany);
+        this.router.post(this.path, this.create);
+        this.router.put(`${this.path}:id`, this.update);
+        this.router.delete(`${this.path}:id`, this.delete);
+    }
+
+    get(req: Request, res: Response): Response {
+        return res.json({
+            message: 'Welcome Home',
+            data: null
+        });
+    }
+    getMany(req: Request, res: Response) {
+        return res.status(405).json({
+            message: '405 Method Not Allowed',
+            data: null
+        });
+    }
+    create(req: Request, res: Response) {
+        return res.status(405).json({
+            message: '405 Method Not Allowed',
+            data: null
+        });
+    }
+    update(req: Request, res: Response) {
+        return res.status(405).json({
+            message: '405 Method Not Allowed',
+            data: null
+        });
+    }
+    delete(req: Request, res: Response) {
+        return res.status(405).json({
+            message: '405 Method Not Allowed',
+            data: null
+        });
     }
 }
